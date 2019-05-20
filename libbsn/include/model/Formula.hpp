e80cdaba09f4407340542b09a8b006db0443fa62
rdinizcal <rdinizcal@gmail.com>
2019-05-20 08:17:49 -0300

    formula

---
+++ libbsn/include/model/Formula.hpp
@@ -0,0 +1,25 @@
+#ifndef FORMULA_HPP
+#define FORMULA_HPP
+
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include "lepton/Lepton.h"
+ 
+namespace simpa {
+    namespace model {
+        class Formula {
+        
+            public:
+                Formula(const std::string& text);
+                ~Formula();
+
+                double apply(const std::vector<std::string> parameters, const std::vector<double> values);
+
+            private:
+                Lepton::CompiledExpression expression;
+        };
+    }
+}
+
+#endif
