d10358ea764ef708f92738301b4a05fb2a3de47d
gabrielevi10 <gabrielevigomes@gmail.com>
2019-05-24 11:54:19 -0300

    Comment controller launch.

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ bsn=$PWD
 gnome-terminal -x roscore & sleep 10s
 
 gnome-terminal --working-directory=${bsn}/launch -e 'roslaunch centralhub.launch'
-gnome-terminal --working-directory=${bsn}/launch -e 'roslaunch controller.launch'
+# gnome-terminal --working-directory=${bsn}/launch -e 'roslaunch controller.launch'
 gnome-terminal --working-directory=${bsn}/launch -e 'roslaunch thermometer.launch'
 gnome-terminal --working-directory=${bsn}/launch -e 'roslaunch ecg.launch'
 gnome-terminal --working-directory=${bsn}/launch -e 'roslaunch oximeter.launch'
