12f63b83efb32698ff93992486e3384104c825c5
gabrielevi10 <gabrielevigomes@gmail.com>
2019-05-24 08:38:02 -0300

    Add launch for controller.

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@ bsn=$PWD
 
 gnome-terminal -x roscore & sleep 10s
 
-gnome-terminal --working-directory=${bsn}/launch -e 'roslaunch centralhub.launch' & sleep 10s
+gnome-terminal --working-directory=${bsn}/launch -e 'roslaunch centralhub.launch'
+gnome-terminal --working-directory=${bsn}/launch -e 'roslaunch controller.launch'
 gnome-terminal --working-directory=${bsn}/launch -e 'roslaunch thermometer.launch'
 gnome-terminal --working-directory=${bsn}/launch -e 'roslaunch ecg.launch'
 gnome-terminal --working-directory=${bsn}/launch -e 'roslaunch oximeter.launch'
