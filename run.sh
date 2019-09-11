2a2ea8e6790ba283d3f61e2706a51899dcfc5984
Jorge Mendes <jorge.jglm@gmail.com>
2019-09-11 12:02:58 -0700

    alteracao do run.sh

--- run.sh
+++ run.sh
@@ -3,17 +3,17 @@ bsn=$PWD
 gnome-terminal -x roscore & sleep 5s
 
 ################# KNOWLEDGE REPOSITORY #################
-gnome-terminal --working-directory=${bsn}/configurations/repository -e 'roslaunch data_access.launch'
+#gnome-terminal --working-directory=${bsn}/configurations/repository -e 'roslaunch data_access.launch'
 
 ################# MANAGER #################
 #gnome-terminal --working-directory=${bsn}/configurations/manager -e 'roslaunch scheduler.launch'
 
 ################# LOGGING INFRASTRUCTURE #################
-gnome-terminal --working-directory=${bsn}/configurations/logging_infrastructure -e 'roslaunch logger.launch'
+#gnome-terminal --working-directory=${bsn}/configurations/logging_infrastructure -e 'roslaunch logger.launch'
 
 ################# APPLICATION #################
-gnome-terminal --working-directory=${bsn}/configurations/application -e 'roslaunch probe.launch'
-gnome-terminal --working-directory=${bsn}/configurations/application -e 'roslaunch effector.launch'
+#gnome-terminal --working-directory=${bsn}/configurations/application -e 'roslaunch probe.launch'
+#gnome-terminal --working-directory=${bsn}/configurations/application -e 'roslaunch effector.launch'
 
 gnome-terminal --working-directory=${bsn}/configurations/application -e 'roslaunch g4t1.launch'
 gnome-terminal --working-directory=${bsn}/configurations/application -e 'roslaunch g3t1_1.launch'
