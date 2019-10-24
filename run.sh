a9229b13a1d6c12e7dc536d7d6aa85d3a4f27791
Gastd <gabriel.araujo.5000@gmail.com>
2019-10-24 16:53:27 -0300

    Add sensor id to SensorData message

--- run.sh
+++ run.sh
@@ -3,13 +3,13 @@ bsn=$PWD
 gnome-terminal -x roscore & sleep 5s
 
 ################# KNOWLEDGE REPOSITORY #################
-#gnome-terminal --working-directory=${bsn}/configurations/knowledge_repository -e 'roslaunch data_access.launch' & sleep 1s
+gnome-terminal --working-directory=${bsn}/configurations/knowledge_repository -e 'roslaunch data_access.launch' & sleep 1s
 
 ################# MANAGER #################
 #gnome-terminal --working-directory=${bsn}/configurations/system_manager -e 'roslaunch enactor.launch'
 
 ################# LOGGING INFRASTRUCTURE #################
-#gnome-terminal --working-directory=${bsn}/configurations/logging_infrastructure -e 'roslaunch logger.launch' & sleep 1s
+gnome-terminal --working-directory=${bsn}/configurations/logging_infrastructure -e 'roslaunch logger.launch' & sleep 1s
 
 ################# APPLICATION #################
 gnome-terminal --working-directory=${bsn}/configurations/target_system -e 'roslaunch probe.launch' & sleep 1s
@@ -18,7 +18,7 @@ gnome-terminal --working-directory=${bsn}/configurations/target_system -e 'rosla
 gnome-terminal --working-directory=${bsn}/configurations/target_system -e 'roslaunch g4t1.launch' & sleep 5s
 gnome-terminal --working-directory=${bsn}/configurations/target_system -e 'roslaunch g3t1_1.launch'
 gnome-terminal --working-directory=${bsn}/configurations/target_system -e 'roslaunch g3t1_2.launch'
-gnome-terminal --working-directory=${bsn}/configurations/target_system -e 'roslaunch g3t1_3.launch'
+gnome-terminal --working-directory=${bsn}/configurations/target_system -e 'roslaunch g3t1_3_double.launch'
 #gnome-terminal --working-directory=${bsn}/configurations/target_system -e 'roslaunch g3t1_4.launch'
 
 ################# SIMULATION #################
