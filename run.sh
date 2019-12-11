a8e6e78472351e4000568d1f306f5a347c53bae7
ericbg27 <ericbgil@gmail.com>
2019-12-11 00:45:16 -0300

    System prepared for experiments

--- run.sh
+++ run.sh
@@ -1,62 +1,35 @@
-#!/bin/bash
-x=1
-cycles=1
-
-while [ $x -le $cycles ]
-do
 bsn=$PWD
 
 gnome-terminal -x roscore & sleep 5s
 
 ################# KNOWLEDGE REPOSITORY #################
-gnome-terminal --working-directory=${bsn}/configurations/knowledge_repository -e 'roslaunch --pid=/var/tmp/data_access.pid data_access.launch' & sleep 1s
+gnome-terminal --working-directory=${bsn}/configurations/knowledge_repository -e 'roslaunch data_access.launch' & sleep 1s
+
 ################# MANAGER #################
-gnome-terminal --working-directory=${bsn}/configurations/system_manager -e 'roslaunch --pid=/var/tmp/enactor.pid enactor.launch'
+gnome-terminal --working-directory=${bsn}/configurations/system_manager -e 'roslaunch enactor.launch'
 
 ################# LOGGING INFRASTRUCTURE #################
-gnome-terminal --working-directory=${bsn}/configurations/logging_infrastructure -e 'roslaunch --pid=/var/tmp/logger.pid logger.launch' & sleep 1s
+gnome-terminal --working-directory=${bsn}/configurations/logging_infrastructure -e 'roslaunch logger.launch' & sleep 1s
 
 ################# REPOSITORY #################
 #gnome-terminal --working-directory=${bsn}/configurations/repository -e 'roslaunch data_access_node.launch'
 #gnome-terminal --working-directory=${bsn}/configurations/repository -e 'roslaunch illness_identifier.launch'
 
 ################# APPLICATION #################
-gnome-terminal --working-directory=${bsn}/configurations/target_system -e 'roslaunch --pid=/var/tmp/probe.pid probe.launch' & sleep 1s
-gnome-terminal --working-directory=${bsn}/configurations/target_system -e 'roslaunch --pid=/var/tmp/effector.pid effector.launch' & sleep 1s
+gnome-terminal --working-directory=${bsn}/configurations/target_system -e 'roslaunch probe.launch' & sleep 1s
+gnome-terminal --working-directory=${bsn}/configurations/target_system -e 'roslaunch effector.launch' & sleep 1s
 
-gnome-terminal --working-directory=${bsn}/configurations/target_system -e 'roslaunch --pid=/var/tmp/g4t1.pid g4t1.launch' & sleep 5s
-gnome-terminal --working-directory=${bsn}/configurations/target_system -e 'roslaunch --pid=/var/tmp/g3t1_1.pid g3t1_1.launch'
-gnome-terminal --working-directory=${bsn}/configurations/target_system -e 'roslaunch --pid=/var/tmp/g3t1_2.pid g3t1_2.launch'
-gnome-terminal --working-directory=${bsn}/configurations/target_system -e 'roslaunch --pid=/var/tmp/g3t1_3.pid g3t1_3.launch'
-gnome-terminal --working-directory=${bsn}/configurations/environment   -e 'roslaunch --pid=/var/tmp/patient.pid patient.launch'
+gnome-terminal --working-directory=${bsn}/configurations/target_system -e 'roslaunch g4t1.launch' & sleep 5s
+gnome-terminal --working-directory=${bsn}/configurations/target_system -e 'roslaunch g3t1_1.launch'
+gnome-terminal --working-directory=${bsn}/configurations/target_system -e 'roslaunch g3t1_2.launch'
+gnome-terminal --working-directory=${bsn}/configurations/target_system -e 'roslaunch g3t1_3.launch'
+gnome-terminal --working-directory=${bsn}/configurations/environment   -e 'roslaunch patient.launch'
 
 #gnome-terminal --working-directory=${bsn}/configurations/target_system -e 'roslaunch g3t1_4.launch'
 
 ################# SIMULATION #################
-gnome-terminal --working-directory=${bsn}/configurations/simulation -e 'roslaunch --pid=/var/tmp/analyzer.pid analyzer.launch' & sleep 1s
+gnome-terminal --working-directory=${bsn}/configurations/simulation -e 'roslaunch analyzer.launch' & sleep 5s
+#gnome-terminal --working-directory=${bsn}/configurations/simulation -e 'roslaunch injector.launch' & sleep 150s
 
-gnome-terminal --working-directory=${bsn}/configurations/simulation -e 'roslaunch --pid=/var/tmp/injector.pid injector.launch'
+gnome-terminal --working-directory=${bsn}/configurations/system_manager -e 'roslaunch engine.launch'
 
-gnome-terminal --working-directory=${bsn}/configurations/system_manager -e 'roslaunch --pid=/var/tmp/engine.pid engine.launch' & sleep 300s
-
-kill $(cat /var/tmp/data_access.pid && rm /var/tmp/data_access.pid) & sleep 1s
-kill $(cat /var/tmp/enactor.pid && rm /var/tmp/enactor.pid) & sleep 1s
-kill $(cat /var/tmp/logger.pid && rm /var/tmp/logger.pid) & sleep 1s
-kill $(cat /var/tmp/probe.pid && rm /var/tmp/probe.pid) & sleep 1s
-kill $(cat /var/tmp/effector.pid && rm /var/tmp/effector.pid) & sleep 1s
-kill $(cat /var/tmp/g4t1.pid && rm /var/tmp/g4t1.pid) & sleep 1s
-kill $(cat /var/tmp/g3t1_1.pid && rm /var/tmp/g3t1_1.pid) & sleep 1s
-kill $(cat /var/tmp/g3t1_2.pid && rm /var/tmp/g3t1_2.pid) & sleep 1s
-kill $(cat /var/tmp/g3t1_3.pid && rm /var/tmp/g3t1_3.pid) & sleep 1s
-kill $(cat /var/tmp/patient.pid && rm /var/tmp/patient.pid) & sleep 1s
-kill $(cat /var/tmp/analyzer.pid && rm /var/tmp/analyzer.pid) & sleep 1s
-kill $(cat /var/tmp/injector.pid && rm /var/tmp/injector.pid) & sleep 1s
-kill $(cat /var/tmp/engine.pid && rm /var/tmp/engine.pid) & sleep 1s
-
-kill $(pgrep roscore)
-
-sleep 15s
-
-x=$(( x + 1 ))
-
-done
--- updateparameters.py
+++ updateparameters.py
@@ -33,46 +33,67 @@ for index,line in enumerate(data):
 	if finished_search == True:
 		break
 
-old_gain = gain
-old_offset = offset
+old_gain = ""
+old_offset = ""
 
-if gain == 0.5:
-	offset += 0.1
-	gain = 0.02
+i = data[gain_index].find('"', data[gain_index].find("v"), len(data[gain_index])-1)
+i = i+1
+while data[gain_index][i] != '"':
+	old_gain += data[gain_index][i]
+	i += 1
 
-	str_offset = str(offset)
+if int(gain) == 5:
+	if offset == 0.01:
+		offset += 0.04
+	else:
+		offset += 0.05
+	offset = round(offset,2)
+	gain = 0.0005
+
+	str_off = str(offset)
 	str_gain = str(gain)
 
-	if len(str(offset)) > 4:
-		str_off = str(offset)[0:4]
-	if len(str(gain)) > 4:
-		str_gain = str(gain)[0:4]
+	if len(str(gain)) > 6:
+		str_gain = str(gain)[0:6]
 
-	data[off_index] = data[off_index].replace(str(old_offset), str_off)
-	data[gain_index] = data[gain_index].replace(str(old_gain), str_gain)
+	i = data[off_index].find('"', data[off_index].find("v"), len(data[off_index])-1)
+	i += 1
+	while data[off_index][i] != '"':
+		old_offset += data[off_index][i]
+		i += 1
+	
+	data[off_index] = data[off_index].replace(old_offset, str_off)
+	data[gain_index] = data[gain_index].replace(old_gain, str_gain)
 else:
-	gain += 0.02
+	if gain < 0.001:
+		gain += 0.0005
+	elif gain < 0.01 and gain >= 0.001:
+		if gain == 0.001:
+			gain += 0.001
+		else:
+			gain += 0.002
+	elif gain < 0.1 and gain >= 0.01:
+		if gain == 0.01:
+			gain += 0.01
+		else:
+			gain += 0.02
+	elif gain >= 0.1 and gain < 0.5:
+		gain += 0.1
+	else:
+		if gain < 2:
+			gain += 0.5
+		else:
+			gain = 5
 
 	str_gain = str(gain)
 
-	if len(str(gain)) > 4:
-		str_gain = str(gain)[0:4]
+	if len(str(gain)) > 6:
+		str_gain = str(gain)[0:6]
 
-	data[gain_index] = data[gain_index].replace(str(old_gain), str_gain)
+	data[gain_index] = data[gain_index].replace(old_gain, str_gain)
 
 fp = open(abs_file_path, "w")
 
 fp.writelines(data)
 
 fp.close()
-
-#r_file_path = "configurations/system_manager/enactor.launch"
-#abs_file_path = os.path.join(bsn_dir,r_file_path)
-
-#f = open(abs_file_path, "r")
-
-#data = f.readlines()
-
-#f.close()
-
-#Fazer para o enactor mas primeiramente deve-se mudar o código do mesmo para receber o Kp pela launch file
