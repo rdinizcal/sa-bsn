bea56a8f22c0346525a3ee7b0d67ba00a7a9ba80
Ricardo Caldas <rdinizcal@gmail.com>
2021-02-20 10:21:19 +0100

    Fixed processor tests; extracted atomic tests from more complex ones.

--- src/libbsn/test/unit/processor/ProcessorTest.cpp
+++ src/libbsn/test/unit/processor/ProcessorTest.cpp
@@ -8,40 +8,56 @@ using namespace bsn::processor;
 
 class ProcessorTest : public testing::Test {
     protected:
-        vector<double> vet;
-        vector<double> empty;
-
-        ProcessorTest() : vet({-1,-1,-1}), empty(3) {}
-
-        virtual void SetUp () {}
-
-        virtual void TearDown () {
-            vet.clear();
-            empty.clear();
-        }
+        ProcessorTest() {}
 };
 
-TEST_F(ProcessorTest, GetSensorId) {
+TEST_F(ProcessorTest, GetThermometerId) {
     ASSERT_EQ(0, get_sensor_id("thermometer"));
+}
+
+TEST_F(ProcessorTest, GetECGId) {
     ASSERT_EQ(1, get_sensor_id("ecg"));
+}
+
+TEST_F(ProcessorTest, GetOxiemeterId) {
     ASSERT_EQ(2, get_sensor_id("oximeter"));
-    ASSERT_EQ(3, get_sensor_id("bpms"));
-    ASSERT_EQ(4, get_sensor_id("bpmd"));
+}
+
+TEST_F(ProcessorTest, GetBPMSId) {
+    ASSERT_EQ(3, get_sensor_id("abps"));
+
+}
+
+TEST_F(ProcessorTest, GetBPMDId) {
+    ASSERT_EQ(4, get_sensor_id("abpd"));
+}
+
+TEST_F(ProcessorTest, GetGlucosemeterId) {
     ASSERT_EQ(5, get_sensor_id("glucosemeter"));
+}
+
+TEST_F(ProcessorTest, GetUnknownId) {
     ASSERT_EQ(-1, get_sensor_id("unknown"));
+    ASSERT_EQ(-1, get_sensor_id("aaah"));
+    ASSERT_EQ(-1, get_sensor_id("thermomemeter"));
 }
 
 TEST_F(ProcessorTest, GetValue) {
     ASSERT_EQ(10.5, get_value("tipo-10.5"));
 }
 
-TEST_F(ProcessorTest, DataFuse) {
+TEST_F(ProcessorTest, FuseEmptyDataVector) {
+    vector<double> vet({});
+
     ASSERT_EQ(-1, data_fuse(vet));
+}
+
+TEST_F(ProcessorTest, FuseThreeValues) {
+    vector<double> vet({1,1,1});
+
     vet.at(0) = 70.0;
     vet.at(1) = 60.0;
     vet.at(2) = 50.0;
 
-    //data_fuse
-    ASSERT_EQ(-1, data_fuse(empty));
     ASSERT_EQ(66,static_cast<int>(data_fuse(vet)));
 }
